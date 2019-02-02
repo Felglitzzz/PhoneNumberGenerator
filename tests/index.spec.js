2373f540a39f0d65ca01e72a1d188b5679c3d3d2
Felglitzzz <felix.amande@andela.com>
2019-02-02 22:20:33 +0100

    chore: set up test and ci

---
+++ tests/index.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-expressions */
+import { expect } from 'chai';
+
+
+describe('smoke test', () => {
+  it('should smoke the test', () => {
+    expect(true).to.be.true;
+  });
+});
